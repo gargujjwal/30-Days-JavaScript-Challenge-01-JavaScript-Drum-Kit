c98b353f6311de0033f94660f2a583eb33f82b91
Ujjwal Garg <ujjwalgarg100204@gmail.com>
2023-06-03 20:55:34 +0530

    implete responsiveness

--- asset/scripts.js
+++ asset/scripts.js
@@ -1,28 +1,34 @@
 "use strict";
 
 function playAudio(keyCode) {
-    const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
-    if (audio) {
-        audio.currentTime = 0;
-        const key = document.querySelector(`div[data-key="${keyCode}"]`);
-        key.classList.toggle("playing");
-        audio.play();
-    }
+	const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+	if (audio) {
+		audio.currentTime = 0;
+		const key = document.querySelector(`div[data-key="${keyCode}"]`);
+		key.classList.toggle("playing");
+		audio.play();
+	}
 }
 
-window.addEventListener("keydown", ({keyCode}) => playAudio(keyCode));
+window.addEventListener("keydown", ({ keyCode }) => playAudio(keyCode));
 
 document.querySelectorAll(".key").forEach(e => {
-    e.addEventListener("click", ({target}) => {
-        playAudio(target.getAttribute("data-key"));
-    });
-    e.addEventListener("transitionend", ({propertyName}) => {
-        if (propertyName === "transform") e.classList.remove("playing");
-    });
-    e.querySelector("kbd").addEventListener("click", ({target: {parentElement}}) => {
-        playAudio(parentElement.getAttribute("data-key"));
-    });
-    e.querySelector("p").addEventListener("click", ({target: {parentElement}}) => {
-        playAudio(parentElement.getAttribute("data-key"));
-    });
+	e.addEventListener("click", ({ target }) =>
+		playAudio(target.getAttribute("data-key"))
+	);
+	e.addEventListener(
+		"transitionend",
+		({ propertyName }) =>
+			propertyName === "transform" && e.classList.remove("playing")
+	);
+	e.querySelector("kbd").addEventListener(
+		"click",
+		({ target: { parentElement } }) =>
+			playAudio(parentElement.getAttribute("data-key"))
+	);
+	e.querySelector("p").addEventListener(
+		"click",
+		({ target: { parentElement } }) =>
+			playAudio(parentElement.getAttribute("data-key"))
+	);
 });
